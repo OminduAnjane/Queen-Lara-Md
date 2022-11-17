d89543a44b29ac8aaac436d3c82f6b631d62655d
Omindu Anjana <93399269+OminduAnjane@users.noreply.github.com>
2022-11-17 06:55:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const { existsSync } = require('fs')
 //═══════[Required Variables]════════\\
 global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://queenlara:Dmoa0817.@queenlara.jz3uzcg.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'ominduanjana@gmail.com'
 global.github = 'https://github.com/OminduAnjane/Queen-Lara-Md'
@@ -18,17 +18,17 @@ module.exports = {
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0], 
   packname: process.env.PACK_INFO.split(";")[1],
-  autoreaction: process.env.AUTO_REACTION || 'off',
+  autoreaction: process.env.AUTO_REACTION || 'on',
   antibadword : process.env.ANTI_BAD_WORD || 'fuck',
   antifake : process.env.FAKE_COUNTRY_CODE || '',
   readmessage: process.env.READ_MESSAGE || true,
   prefix: process.env.PREFIX || ['.' , '$'],
-  nsfw_detect_ai : process.env.NSFW_DETECTION_AI || 'false',
+  nsfw_detect_ai : process.env.NSFW_DETECTION_AI || 'true',
   pmpermit: process.env.PMPERMIT || "false",
-  mongodb_url: process.env.MONGODB_URI || "Enter-Mongo-URI-HERE",
+  mongodb_url: process.env.MONGODB_URI || "mongodb+srv://queenlara:Dmoa0817.@queenlara.jz3uzcg.mongodb.net/?retryWrites=true&w=majority",
   warncount : process.env.WARN_COUNT || 3,
   disablepm: process.env.DISABLE_PM || "flase",
-  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'true',
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
   antilinkaction: process.env.ANTILINK_ACTION || 'kick',
   HEROKU: {
@@ -37,7 +37,7 @@ module.exports = {
         APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'zeropgg' : process.env.HEROKU_APP_NAME
     },
   BRANCH: 'main',
-  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.5' : process.env.VERSION,
  LANG: process.env.THEME|| 'LARALK',
  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
